741c5f91d41b8106b55ae758f28af6d41056a8e6
LucasDusatko <Lucas.Dusatko@gmail.com>
2016-12-28 15:01:26 -0800

    Add project files.

---
+++ IdentityTestProject/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNet.Identity.Owin;
+using System.Web.Mvc;
+using IdentityTestProject.Infrastructure;
+using System.Web;
+
+namespace IdentityTestProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: List of users
+        public ActionResult Index()
+        {
+
+            return View(UserManager.Users);
+        }
+
+
+
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+    }
+}
